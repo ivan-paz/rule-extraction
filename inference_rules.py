8d8cee8d9216bd8d26e31926b0be1fb6c7360fcf
ivan-paz <ivnpaz@gmail.com>
2016-12-13 14:50:07 +0100

    modify interval function

--- inference_rules.py
+++ inference_rules.py
@@ -12,8 +12,15 @@ presets = (
 (1, 4,'B',1),
 (1,11,'A',1),
 (1,12,'A',1) )
-
 rules = [(1, 4, 'B', 1), ((1, 2), 1, 'A', 1), (1, (1, 3, 11, 12), 'A', 1)]
+#-----------------------------------------------------------------------------------
+presets = (
+(1, 1, 1, 'A', 1),
+(1, 2, 1, 'A', 1),
+(1, 4, 1, 'A', 1),
+(2, 1, 1, 'A', 1),
+(1, 3, 1, 'B', 1) )
+rules =  [(1, 3, 1, 'B', 1), ((1, 2), 1, 1, 'A', 1), (1, (1, 2, 4), 1, 'A', 1)]
 
 def build_inference_rules(rules,presets):
     inference_rules = []
@@ -21,15 +28,12 @@ def build_inference_rules(rules,presets):
     presets = presets_by_classes(presets)
     for key in _rules_by_classes:
         for rule in _rules_by_classes[key]:
-            tmp = build_rules(inference_dict(rule,presets))
+            #print(key,rule)
+            tmp = build_rules( inference_dict( rule, presets ) )
             for rule in tmp: inference_rules.append(rule)
     return inference_rules
     
 build_inference_rules(rules,presets)
-
-
-
-
 
 import itertools
 def build_rules(inference_dict):
@@ -60,6 +64,8 @@ def build_rules(inference_dict):
     
 #build_rules({0: [(1, 2)], 1: 1})
 #build_rules({0: 1, 1: [(1, 3), (11, 12)]})
+#inference_dict((1, (1, 2, 4), 1, 'A', 1), pbc)
+#3pbc = presets_by_classes(presets)
 
 def inference_dict(rule,presets_by_classes):
     inference_dict = {}
@@ -68,9 +74,9 @@ def inference_dict(rule,presets_by_classes):
             _class = rule[-2]  # Change this if the format changes
             different_keys = []
             for key in presets_by_classes:
-                if key!= _class:
+                if key != _class:
                     different_keys.append(presets_by_classes[key])
-            new_interval = modify_interval(rule[i],i,different_keys )
+            new_interval = modify_interval( rule[i],i, different_keys )
             inference_dict[i]= new_interval
         else:
             inference_dict[i]=rule[i]
@@ -79,22 +85,24 @@ def inference_dict(rule,presets_by_classes):
 def modify_interval(interval, i, different_keys):
     new_interval = []
     temp_interval = []
-    for j in range(len(interval) - 1 ):
+    for j in range( len(interval) - 1 ):
         a = interval[j]
         b = interval[j + 1]
         #print(a,b)
         temporal = set()
         for dictionary in different_keys:
             for preset in dictionary:
-               # print(preset[i])
                 if not (a < preset[i] < b):
                     temporal.add(a)
                     temporal.add(b)
-        if temporal!= set():
+                elif ( a < preset[i] < b ) and (j == len(interval) - 2):
+                    temporal.add(b)
+        if temporal != set():
             temp_interval.append(temporal)
     for i in temp_interval: new_interval.append(tuple(i))
     return new_interval
-
+    
+#-------------------------------------------------------------------------------
 def rules_by_classes(rules):
     rules_by_classes = {}
     for rule in rules:
@@ -106,7 +114,6 @@ def rules_by_classes(rules):
             rules_by_classes[_class].append(rule)
     return rules_by_classes
 #rules_by_classes(rules)
-
 def presets_by_classes(presets):
     presets_by_classes = {}
     for preset in presets:
