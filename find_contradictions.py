6878e43b38a38a52d64e0ea288a406cc878890a4
ivan-paz <ivnpaz@gmail.com>
2017-01-31 16:03:11 +0100

    building inference fules function

--- find_contradictions.py
+++ find_contradictions.py
@@ -5,7 +5,26 @@ Input: rule,others
 rule: rule to be extended into inference rule
 others: presets_different_class
 """
+presets = [
+[1,4,'A',1],
+[1,6,'A',1],
+[2,4,'A',1],
+[2,6,'A',1],
+[3,4,'A',1],
+[3,6,'A',1],
+[8,4,'A',1],
+[8,6,'A',1],
+[11,4,'A',1],
+[11,6,'A',1],
+[5,4,'B',1]
+]
+from rulex_1 import *
+rules = rulex(presets)
+rules = rulex(rules)
+rules = [[5, 4, 'B', 1], ((1, 2, 3, 8, 11), (4, 6), 'A', 1)]
 
+
+"""
 rule =  ((5.504405, 10.203962, 11.354432), 20, 0.6, 'A', 1)
 others = [
 [6, 20, 0.6, 'D', 1],
@@ -20,15 +39,43 @@ others = [
 [21.10586, 370, 0.6, 'B', 1],
 [21.10586, 26, 0.6, 'B', 1]
 ]
+"""
+rule = ((1, 2, 3, 8, 11), (4, 6), 'A', 1)
+presets_different_class = [[5,4,'B',1]]
 
-
-def inference_rule(rule,presets_different_class):
+def find_contradictions(rule,presets_different_class):
     for i in range(len(rule) -2 ):
         if type(rule[i])==tuple:
             print(rule[i])
+            #COMPARE
             #find if there are identical rules differing
             #in that parameter
-inference_rule(rule,others)
+find_contradictions(rule,presets_different_class)
 
-# #find if there are identical rules differing
-            #in that parameter
+
+#find if there are identical presets differin in that parameter
+def compare_with_presets(i,rule,presets_different_class):
+    rule = cut_rule(i,rule)
+    for preset in presets_different_class:
+        preset = cut_preset(i,preset)
+        print(rule, preset)
+        #pairwise comparison
+
+compare_with_presets(0,((1, 2, 3, 8, 11), (4, 6), 'A', 1),[[5,4,'B',1]])
+
+
+def cut_rule(i,rule):
+    rule = list(rule)
+    rule = rule[:-2]
+    del rule[i]
+    return rule
+cut_rule(0,((1, 2, 3, 8, 11), (4, 6), 'A', 1))
+
+def cut_preset(i,preset):
+    preset = preset[:-2] #Change this if format changes
+    del preset[i]
+    return preset
+cut_preset(0,[5,4,'B',1])
+
+
+
